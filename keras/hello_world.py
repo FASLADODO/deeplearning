8c420460c5fc0e249c8532ed353f353cfc4a56b4
Jadson Santos <jadson@info.ufrn.br>
2018-05-25 18:49:37 -0300

    keras
    Signed-off-by: Jadson Santos <jadson@info.ufrn.br>

--- keras/hello_world.py
+++ keras/hello_world.py
@@ -1,9 +1,1 @@
-from keras.models import Sequential
-from keras.layers import Dense, Activation
-
-model = Sequential([
-    Dense(32, input_shape=(784,)),
-    Activation('relu'),
-    Dense(10),
-    Activation('softmax'),
-])
+import keras
