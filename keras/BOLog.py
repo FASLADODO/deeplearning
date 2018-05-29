6c670140fe7b75ac5b6db3a0ab54f93341644543
Jadson Santos <jadson@info.ufrn.br>
2018-05-29 19:24:44 -0300

    LSTM input
    Signed-off-by: Jadson Santos <jadson@info.ufrn.br>

--- keras/BOLog.py
+++ keras/BOLog.py
@@ -3,7 +3,24 @@
 
 from bo_log_csv_module import load_data
 
+from bo_input_module import prepared
 
 data_train = load_data('/Users/jadson/Desktop/trainingmock.csv', [0,2,3,4]);
 print(data_train)
 print(len(data_train))
+
+
+''' 
+    https://github.com/keras-team/keras/issues/2045
+    input data should be 3D tensor with shape (nb_samples, timesteps, input_dim).
+
+    If I have 1000 sentences,each sentence has 10 words, and each word is presented in a 3-dim vector, so:
+    1000 is nb_samples, 
+    10 is the timesteps and 
+    3 is the input_dim
+
+
+    https://machinelearningmastery.com/reshape-input-data-long-short-term-memory-networks-keras/
+
+
+'''
