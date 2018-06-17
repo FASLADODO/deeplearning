45cb40278d2b4eee5576699dba3725eb50feb903
jadsonjs <jadsonjs@gmail.com>
2018-06-17 10:54:46 -0300

    lstm running
    Signed-off-by: jadsonjs <jadsonjs@gmail.com>

--- business-op-deeplog/business_op_lstm.py
+++ business-op-deeplog/business_op_lstm.py
@@ -24,6 +24,7 @@
 
 from __future__ import print_function
 
+import numpy as np
 from numpy import array
 
 from csv_module import load_csv_data
@@ -31,6 +32,8 @@ from csv_module import load_csv_data2
 from array_module import reshape_input
 
 from prepare_input_module import prepareLSTMdata
+
+from process_learning_module import buildLSTMModel
 from process_learning_module import processLSTMLearning
 
 
@@ -44,10 +47,15 @@ deep_network_file_name = '/Users/jadson/Desktop/trainingdata.csv';
 #raw_data_file_name = '/home/jadson/Documentos/deeplog/csvs/tinytrainingmock2.csv';
 #deep_network_file_name = '/home/jadson/Documentos/deeplog/csvs/trainingdata.csv';
 
-qtd_samples = 1 
-qtd_timesteps = 10
-qtd_features = 5
+samples = 1 
+timesteps = 10
+features = 5
 features_indexes = [0,1,2,3,5]
+
+lstm_layer_size = 32
+dence_layer_size = 10
+batch_size= 64
+epochs=10
 # ======================================================
 
 
@@ -60,14 +68,14 @@ features_indexes = [0,1,2,3,5]
 cvs_raw_data = load_csv_data2(deep_network_file_name)
 #print(cvs_raw_data)
 
-lstm_input_data = reshape_input(cvs_raw_data, qtd_samples, qtd_timesteps, qtd_features)
+x_train = reshape_input(cvs_raw_data, samples, timesteps, features)  # 1 x 10 x 5
 #print('### 4 ###')
 #print(lstm_input_data)
 
-lstm_input_label = [0.1]
+y_train = np.array( [ [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0] ] ) # 1 x 10
 
 # 5 features, 10 timesteps
-lstm_test_data = array([
+x_test = array([
   [0.1, 1.0, 0.1, 1.0, 0.0],
   [0.2, 0.9, 0.1, 1.0, 0.0],
   [0.3, 0.8, 0.1, 1.0, 0.0],
@@ -79,21 +87,44 @@ lstm_test_data = array([
   [0.9, 0.2, 0.1, 1.0, 0.0],
   [1.0, 0.1, 0.1, 1.0, 0.0]])
 
-lstm_test_data = reshape_input(lstm_test_data, qtd_samples, qtd_timesteps, qtd_features)
+x_test = reshape_input(x_test, samples, timesteps, features) # 1 x 10 x 5
 
-lstm_test_label =  [0.1]
+y_test =  np.array( [ [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0] ] ) # 1 x 10
+
+
+# 5 features, 10 timesteps
+x_val = array([
+  [0.1, 1.0, 0.1, 1.0, 0.0],
+  [0.2, 0.9, 0.1, 1.0, 0.0],
+  [0.3, 0.8, 0.1, 1.0, 0.0],
+  [0.4, 0.7, 0.1, 1.0, 0.0],
+  [0.5, 0.6, 0.1, 1.0, 0.0],
+  [0.6, 0.5, 0.1, 1.0, 0.0],
+  [0.7, 0.4, 0.1, 1.0, 0.0],
+  [0.8, 0.3, 0.1, 1.0, 0.0],
+  [0.9, 0.2, 0.1, 1.0, 0.0],
+  [1.0, 0.1, 0.1, 1.0, 0.0]])
+
+x_val = reshape_input(x_val, samples, timesteps, features) # 1 x 10 x 5
+
+y_val =  np.array( [ [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0] ] )  # 1 x 10
 
 
 # ==================== Process LSTM Model ===============
 
 
-print('--- X_train ---')
-print(lstm_input_data)
-print(len(lstm_input_data))
-print('--- y_train ---')
-print(lstm_input_label)
-print(len(lstm_input_label))
+#print('--- X_train ---')
+#print(x_train)
+#print(len(x_train))
+#print('--- y_train ---')
+#print(y_train)
+#print(len(y_train))
 
 
-processLSTMLearning(lstm_input_data, lstm_input_label, lstm_test_data, lstm_test_label, qtd_timesteps, qtd_features)
+model = buildLSTMModel(lstm_layer_size, dence_layer_size, timesteps, features)
+
+processLSTMLearning(model, batch_size, epochs, x_train, y_train, x_val, y_val, x_test, y_test)
+
+
+
 
--- business-op-deeplog/process_learning_module.py
+++ business-op-deeplog/process_learning_module.py
@@ -7,8 +7,8 @@ from keras.layers import LSTM
 from keras.datasets import imdb
 
 
+def buildLSTMModel(lstm_layer_size, dence_layer_size, timesteps, data_dim):
 
-def processLSTMLearning(lstm_input_data, lstm_input_label, lstm_test_data, lstm_test_label, qtd_timesteps, qtd_features):
 
 	model = Sequential()
 
@@ -26,26 +26,29 @@ def processLSTMLearning(lstm_input_data, lstm_input_label, lstm_test_data, lstm_
 	# return_sequences=True    What this does is ensure that the LSTM cell returns all of 
 	#                          the outputs from the unrolled LSTM cell through time.
 	#
-	model.add( LSTM(10, input_shape=(qtd_timesteps, qtd_features), return_sequences=True ) )
-	model.add( LSTM(10, return_sequences=True) )
-	model.add( LSTM(10, return_sequences=True) )
-
-	#The problem is that you start with a three dimensional layer but 
-	#never reduce the dimensionality in any of the following layers.
-	#Try adding mode.add(Flatten()) before the last Dense layer
-	#
-	# https://github.com/keras-team/keras/issues/6351
-	#
-	model.add(Flatten())
+	model.add( LSTM(lstm_layer_size, return_sequences=True, input_shape=(timesteps, data_dim) ) )
+	model.add( LSTM(lstm_layer_size, return_sequences=True) )
+	model.add( LSTM(lstm_layer_size) )
 
 	# full conected layer a "normal" neural network
 	#model.add(Dense(128, activation='relu'))
-	model.add(Dense(1, activation='softmax'))
+	model.add(Dense(dence_layer_size, activation='softmax'))
+
+	return model;
+
+
+def processLSTMLearning(model, batch_size, epochs, x_train, y_train, x_val, y_val, x_test, y_test):
 
 
 	# try using different optimizers and different optimizer configs
-	model.compile(loss='sparse_categorical_crossentropy',
-	              optimizer='adam',
+	# loss:
+	# https://en.wikipedia.org/wiki/Loss_function
+	# http://ml-cheatsheet.readthedocs.io/en/latest/loss_functions.html
+	# optimizer:
+	# http://ruder.io/optimizing-gradient-descent/index.html#rmsprop
+	#
+	model.compile(loss='categorical_crossentropy',
+	              optimizer='rmsprop',
 	              metrics=['accuracy'])
 
 
@@ -58,14 +61,15 @@ def processLSTMLearning(lstm_input_data, lstm_input_label, lstm_test_data, lstm_
 	#          validation_data=(lstm_test_data, lstm_test_data))
 
 	# without validation data
-	model.fit(lstm_input_data, lstm_input_label, epochs=10, batch_size=1)
+	model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_val, y_val) )
 
+    
 	#score, acc = model.evaluate(lstm_test_data, lstm_test_data,
 	#                            batch_size=batch_size)
 	#print('Test score:', score)
 	#print('Test accuracy:', acc)
 
-	scores = model.evaluate(lstm_test_data, lstm_test_label, verbose=0)
+	scores = model.evaluate(x_test, y_test, verbose=0)
 	print("Accuracy: %.2f%%" % (scores[1]*100))
 
 
